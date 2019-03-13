fe613fbdc1902bf1809612af178f04769b45e35a
rdev06 <46485420+rdev06@users.noreply.github.com>
2019-03-13 18:21:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT ||3000;
+
+//body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.post('/api/databanks/meter',(req,res)=>{
+    res.json({details:req.body});
+    logger.info(req.body);
+    console.log(req.body)
+})
+
+app.listen(port, () => console.log(`server is running at http://localhost:${port}`));
