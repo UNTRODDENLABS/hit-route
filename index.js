9670b66e9a930787228bddfbc4dd7297bbccb7d6
rdev06 <46485420+rdev06@users.noreply.github.com>
2019-03-14 13:08:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
-
+app.get('/',(req,res)=>{
+   res.send('this is hitting message route you need to hit your details as a post request on host+/api/databanks/meter ') 
+});
 app.post('/api/databanks/meter',(req,res)=>{
     res.json({details:req.body});
     console.log(req.body)
